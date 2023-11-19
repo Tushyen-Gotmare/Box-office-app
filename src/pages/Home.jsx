f4ccc40f128d96dcd5a6a1f2198be89663707c5d
Tushyen <tanmaygotmare@gmail.com>
2023-11-19 11:47:55 +0530

    api added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import {useState} from 'react';
+import { useState } from 'react';
+
+import { searchForShows } from "./../api/tvmaze";
+// import { render } from 'react-dom';
 
 const Home = () => {
   
   const [searchStr, setSearchStr] = useState("");
-
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
 
   const onSearchInputChange = (ev) => {
@@ -12,11 +16,42 @@ const Home = () => {
 
   const onSearch = async (ev) =>{
     ev.preventDefault();
-    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`);
-    const body = await response.json();
 
-    console.log(body);
+
+    try{
+      setApiDataError(null);
+      const result =  await searchForShows(searchStr);
+      setApiData(result);
+    }
+    catch (error) {
+      setApiDataError(error);
+    }
+  
+
+
   };
+
+
+  const renderApiData = () => {
+     
+
+
+    if (apiDataError){
+      return <div>Error occured: {apiDataError.message}</div>
+    }
+
+
+    if (apiData) {
+      return apiData.map((data) => (
+          <div key={data.show.id}>{data.show.name}</div>
+        ));
+    }
+    return null ;
+    
+  };
+
+
+
   return (
     <div>
      <form onSubmit={onSearch}>
@@ -25,6 +60,10 @@ const Home = () => {
     <button type="submit" >Search</button>
 
     </form>
+    <div>
+      {renderApiData()}
+      
+    </div>
     </div>
   );
 };
